2dd9be7a900d3fc198ef5d36acc6f39e3b48a65d
Ken Siprell <ken.siprell@gmail.com>
2013-05-18 18:46:55 +0200

    flexisuselect working as planned

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
 	plugins {
 		runtime ":jquery:1.8.3"
 
-		runtime ":resources:1.2.RC3"
+		runtime ":resources:1.2"
 
 		build ":release:2.2.0", ":rest-client-builder:1.0.3", {
 			export = false
