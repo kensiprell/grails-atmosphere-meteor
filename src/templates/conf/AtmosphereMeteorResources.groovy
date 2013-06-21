c4afe25c81c4b4fdf1145a9afcaaf102a911647b
Ken Siprell <ken.siprell@gmail.com>
2013-06-21 18:57:36 +0200

    changed to atmosphere.js

--- src/templates/conf/AtmosphereMeteorResources.groovy
+++ src/templates/conf/AtmosphereMeteorResources.groovy
@@ -1,7 +1,7 @@
 modules = {
 	'atmosphere-meteor' {
 		dependsOn 'jquery'
-		resource id: 'js', url: [plugin: 'atmosphere-meteor', dir: 'js', file: "jquery.atmosphere.js"],
+		resource id: 'js', url: [plugin: 'atmosphere-meteor', dir: 'js', file: "atmosphere.js"],
 				disposition: 'head', nominify: true
 	}
 }
