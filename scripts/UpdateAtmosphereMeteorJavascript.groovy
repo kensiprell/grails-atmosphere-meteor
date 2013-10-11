572258257e9579cf591816b90ce2f5eab605a7fe
Ken Siprell <ken.siprell@gmail.com>
2013-10-11 17:46:23 +0200

    added update-atmosphere-meteor-javascript

---
+++ scripts/UpdateAtmosphereMeteorJavascript.groovy
@@ -0,0 +1,15 @@
+includeTargets << grailsScript("_GrailsInit")
+
+target(updateAtmosphereMeteorJavascript: "Update the atmosphere-meteor plugin Javascript files") {
+	def dir = "${atmosphereMeteorPluginDir}/web-app/js"
+	def javascriptUrl = "https://raw.github.com/Atmosphere/atmosphere-javascript/master/modules/javascript/src/main/webapp/javascript/atmosphere.js"
+	def jqueryUrl = "https://raw.github.com/Atmosphere/atmosphere-javascript/master/modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js"
+
+	ant.get(usetimestamp: true, src: javascriptUrl, dest: dir)
+	ant.get(usetimestamp: true, src: jqueryUrl, dest: dir)
+
+	println "Updated the atmosphere-meteor plugin Javascript files"
+}
+
+setDefaultTarget(updateAtmosphereMeteorJavascript)
+
