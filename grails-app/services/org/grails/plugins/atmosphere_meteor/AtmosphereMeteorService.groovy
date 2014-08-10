dbe03c5ea5e374016d3b2829de6127bf4c1cf5eb
Ken Siprell <ken.siprell@gmail.com>
2014-08-10 19:20:45 +0200

    add AtmosphereMeteorService

---
+++ grails-app/services/org/grails/plugins/atmosphere_meteor/AtmosphereMeteorService.groovy
@@ -0,0 +1,34 @@
+package org.grails.plugins.atmosphere_meteor
+
+import org.atmosphere.cpr.AtmosphereFramework
+import org.atmosphere.cpr.Broadcaster
+import org.atmosphere.cpr.DefaultBroadcaster
+import org.grails.plugins.atmosphere_meteor.AtmosphereConfigurationHolder
+
+class AtmosphereMeteorService {
+
+	/**
+	 * Broadcasts a message to a client or clients, first creating the broadcaster if it does not exist.
+	 * @param mapping  a String that denotes the URL path to the client or clients
+	 * @param data  can be JSON, String, or whatever your client accepts
+	 */
+	def broadcast(String mapping, data) {
+		broadcastData(DefaultBroadcaster.class, mapping, data, true)		
+    }
+
+	/**
+	 * Broadcasts a message to a client or clients.
+	 * @param mapping  a String that denotes the URL path to the client or clients
+	 * @param data  can be JSON, String, or whatever your client accepts
+	 * @param create  a Boolean, true will create the broadcaster if it does not exist and false will not 
+	 */
+   def broadcast(String mapping, data, Boolean create) {
+		broadcastData(DefaultBroadcaster.class, mapping, data, create)		
+    }
+	
+	private broadcastData(clazz, String mapping, data, boolean create) {
+		AtmosphereFramework framework = AtmosphereConfigurationHolder.framework
+		Broadcaster broadcaster = framework.getBroadcasterFactory().getDefault().lookup(clazz, mapping, create)
+		broadcaster.broadcast(data)
+	}
+}
